ca6d60f5979562b481adc91a8c3a5b3eb14b5657
mindhacker <mindhacker1098@gmail.com>
2023-02-26 14:39:48 +0530

    first

---
+++ serv.js
@@ -0,0 +1,101 @@
+const mongoose = require('mongoose');
+const express=require('express');
+const app=express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+mongoose.set('strictQuery', true);
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  }
+mongoose.connect(process.env.m_url).then(()=>{console.log("sucessful")}).catch((err)=>{console.log(err)});
+
+
+
+
+
+
+
+
+
+
+const UserSchema = mongoose.Schema({
+    mainurl: {
+        type: String,
+        required: true
+    },
+    sorturl: {
+        type: String,
+        required: true
+    }
+   
+
+  
+
+})
+var model=mongoose.model('urls',UserSchema)
+app.post('/', async (req, res) => {
+console.log(req.body)
+model.count({},(err,count)=>{
+console.log(count);
+
+var obj={
+
+    mainurl:req.body.name,
+    sorturl:`https://sort.onrender.com/${count+1}`
+    
+    }
+    
+
+
+    const o_url = new model(obj)
+    const ans=o_url.save((err)=>{
+
+
+        if(err){
+            res.send("error") 
+        }
+        else{
+            res.send(obj.sorturl)
+            
+        }
+
+
+    })
+    
+
+});
+
+
+  
+
+
+})
+
+app.get('/:name',async (req,res)=>{
+
+var surl;
+
+model.find({sorturl:"https://sort.onrender.com/"+req.params.name},(err,item)=>{
+    try{
+
+surl=item[0].mainurl;
+res.redirect(surl);}
+catch(exp){
+    res.status(408);
+    res.send("page not found")
+}
+
+})
+
+
+})
+
+
+var port=process.env.PORT || 8000
+app.listen(port,()=>{
+
+    console.log("server started")
+})
+
